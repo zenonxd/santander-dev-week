d59252b92f0194f0731e0de40af0bfb24e8bb391
zenonxd <olavomoreira99@gmail.com>
2024-06-08 00:31:39 -0300

    Final

---
+++ src/main/java/me/dio/controller/UserControler.java
@@ -0,0 +1,34 @@
+package me.dio.controller;
+
+import me.dio.domain.model.User;
+import me.dio.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+public class UserControler {
+    private final UserService userService;
+
+    public UserControler(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        var user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<User> create(@RequestBody User userToCreate) {
+        var userCreated = userService.create(userToCreate);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userCreated.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(userCreated);
+    }
+}
